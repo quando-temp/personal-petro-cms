2318c3dcb4e638af83b664a38ab7d8bd68fc5ee2
at-quando <quan.do@asiantech.vn>
2018-12-17 17:33:34 +0700

    deploy

--- config/puma.rb
+++ config/puma.rb
@@ -13,3 +13,33 @@ on_worker_boot do
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+
+
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
+
+# Change to match your CPU core count
+
+# # Logging
+# stdout_redirect "#{shared_dir}/tmp/log/puma.stdout.log", "#{shared_dir}/tmp/log/puma.stderr.log", true
+
+# # Set master PID and state locations
+# pidfile "#{shared_dir}/tmp/pids/puma.pid"
+# state_path "#{shared_dir}/tmp/pids/puma.state"
+# activate_control_app
+
+# on_worker_boot do
+#   require "active_record"
+#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+# end
