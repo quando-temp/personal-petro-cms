de81c71b458e8ae48e8d246a188d318b43e1bc8f
quan do <quan.do@asiantech.vn>
2019-01-13 15:16:44 +0700

    123

--- config/puma.rb
+++ config/puma.rb
@@ -31,15 +31,15 @@ plugin :tmp_restart
 # Change to match your CPU core count
 
 # # Logging
-# stdout_redirect "#{shared_dir}/tmp/log/puma.stdout.log", "#{shared_dir}/tmp/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/tmp/log/puma.stdout.log", "#{shared_dir}/tmp/log/puma.stderr.log", true
 
 # # Set master PID and state locations
-# pidfile "#{shared_dir}/tmp/pids/puma.pid"
-# state_path "#{shared_dir}/tmp/pids/puma.state"
-# activate_control_app
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
+activate_control_app
 
-# on_worker_boot do
-#   require "active_record"
-#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-#   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-# end
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
