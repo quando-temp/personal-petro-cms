40169d36150b8f2e27ed2695c81c64cf9b93c9af
quan do <quan.do@asiantech.vn>
2019-05-23 08:31:17 +0700

    1234

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,9 @@
 class CallsController < ApplicationController
   def index
-    @usually_customers = Customer.where(type_customer: 0).where.not(id: [Customer.joins(:petros).where("petros.day_fuel > ?", 5.days.ago).ids])
-    @rarely_customers = Customer.where(type_customer: 1).where.not(id: [Customer.joins(:petros).where("petros.day_fuel > ?", 15.days.ago).ids])
+    @usually_customers = Petro.joins(:customer).where("type_customer = 0").where.not(customer_id: [Customer.joins(:petros).where("petros.day_fuel > ?", 5.days.ago).ids])
+    @rarely_customers = Petro.joins(:customer).where("type_customer = 1").where.not(customer_id: [Customer.joins(:petros).where("petros.day_fuel > ?", 15.days.ago).ids])
+    # @usually_customers = Customer.where(type_customer: 0).where.not(id: [Customer.joins(:petros).where("petros.day_fuel > ?", 5.days.ago).ids])
+    # @rarely_customers = Customer.where(type_customer: 1).where.not(id: [Customer.joins(:petros).where("petros.day_fuel > ?", 15.days.ago).ids])
+    # binding.pry
   end
 end
