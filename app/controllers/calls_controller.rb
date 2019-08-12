234d8c036a3ebe4cc0e6591a1a290cc845bd740a
quan do <quan.do@asiantech.vn>
2019-08-12 15:48:24 +0700

    date

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -6,7 +6,7 @@ class CallsController < ApplicationController
     usually_customers_ids = Petro.where("day_fuel > ?", 5.days.ago).pluck(:customer_id).uniq
     @usually_customers = Customer.includes(petro: [:calltimes]).where("type_customer = 0").where.not(id: usually_customers_ids).order("petros.day_fuel DESC").page(params[:page])
     rarely_customers_ids = Petro.where("day_fuel > ?", 15.days.ago).pluck(:customer_id).uniq
-    @rarely_customers = Customer.includes(petro: [:calltimes]).where("type_customer = 1").where.not(id: rarely_customers_ids).order("petros.day_fuel DESC").page(params[:page])
+    @rarely_customers = Customer.includes(petro: [:calltimes]).where("type_customer = 1").where.not(id: rarely_customers_ids).order("petros.day_fuel DESC").page(params[:page_rare])
 
 
     # @usually_customers = Customer.where(type_customer: 0).where.not(id: [Customer.joins(:petros).where("petros.day_fuel > ?", 5.days.ago).ids])
